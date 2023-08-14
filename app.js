3103bffb5c4fad6bab36d5c29bf2cb04bd82aa52
ramox-mq <daniel.rsi6oa@gmail.com>
2023-08-13 23:33:39 -0300

    agregue las rutas faltantes

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+})
+
+
 app.listen(3001, () => {
     console.log('Servidor corriendo en el puerto 3001');
 });
